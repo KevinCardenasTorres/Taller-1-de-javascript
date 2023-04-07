8b20a8f07e7705a737f8e791fa86754a525236c1
KevinCardenasTorres <126635237+KevinCardenasTorres@users.noreply.github.com>
2023-04-06 19:19:12 -0500

    Add files via upload

---
+++ Taller 1 Javascript/proceso.js
@@ -0,0 +1,29 @@
+// Se definen dos variables con valores diferentes
+
+var a = 859
+
+var b = 3791
+
+
+// Se utiliza el condicional si(if) para encontrar la variable con el mayor numero
+
+if (a > b )  {console.log(`El número a  ${a} es mayor que b ${b}` );} 
+
+else if (b > a) {console.log("El número b " + b + " es mayor que a " + a );} 
+
+else {console.log("Los números son iguales");}
+
+// Ahora ahi que asignar valores a tres variables
+let c = 213;
+let d = 394;
+let e = 891;
+
+// Se utiliza el condicional si(if) para tomar decisiones y encontrar la variable con el mayor numero
+
+if (c > d && c > e) {console.log("La variable c tiene el mayor valor: " + c);} 
+
+else if (d > c && d > e) {console.log("La variable d tiene el mayor valor: " + d);}
+
+else { console.log("La variable e tiene el mayor valor: " + e);}
+
+
